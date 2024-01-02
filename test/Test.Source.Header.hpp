70c24ebfeefaaafe4f100f25319622fe9e985497
Grigore Stefan <g_stefan@yahoo.com>
2024-01-02 22:49:49 +0200

    Update

--- test/Test.Source.Header.hpp
+++ test/Test.Source.Header.hpp
@@ -1,5 +1,5 @@
 // Grammar Compiler Test
 // Created by Grigore Stefan <g_stefan@yahoo.com>
 // Public domain (Unlicense) <http://unlicense.org>
-// SPDX-FileCopyrightText: 2022-2023 Grigore Stefan <g_stefan@yahoo.com>
+// SPDX-FileCopyrightText: 2022-2024 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: Unlicense
